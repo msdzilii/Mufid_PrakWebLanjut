96215621eb8ad501cfad1bc360ec413aa3bc3da3
msdzilii <83410300+msdzilii@users.noreply.github.com>
2022-09-06 18:01:28 +0700

    LanjutPrak2

---
+++ app/Database/Seeds/Mahasiswa.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+use CodeIgniter\I18n\Time;
+
+class Mahasiswa extends Seeder
+{
+    public function run()
+    {
+        $data_mahasiswa = [
+            [
+                'npm' => '2017051011',
+                'nama'    => 'Muhammad Mufid Sadzili',
+                'alamat' => 'Pringsewu',
+                'created_at' =>Time :: now(),
+            ],
+            [
+                'npm' => '2017051008',
+                'nama'    => 'Kayla Atsila Ivanka',
+                'alamat' => 'Palembang',
+                'created_at' =>Time :: now(),
+            ],
+            [
+                'npm' => '2017051035',
+                'nama'    => 'Irfan Saputra',
+                'alamat' => 'Terbanggi Besar',
+                'created_at' =>Time :: now(),
+            ]
+        ];
+
+        // Using Query Builder
+        foreach($data_mahasiswa as $data){
+            $this->db->table('mahasiswa')->insert($data);
+        }
+    }
+}
